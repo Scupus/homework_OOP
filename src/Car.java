9142d5361dcbe4f65adae71bda6b7a996b1d5930
Alex <loalantref@hmail.com>
2023-01-14 13:34:22 +1200

    В класс Car добавлены исправления по дефолту в случае полей null, пустых строк и неверных данных (3 часть ДЗ)

--- src/Car.java
+++ src/Car.java
@@ -10,16 +10,6 @@ public class Car {
     private final long id = count;
     private static long count = 1L;
 
-    public Car(String brand, String model, float engineVolume, String color, int year, String country) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
-        this.country = country;
-        count++;
-    }
-
     public String getBrand() {
         return brand;
     }
@@ -68,26 +58,59 @@ public class Car {
         this.country = country;
     }
 
+    public long getId() {
+        return id;
+    }
+
     public static long getCount() {
         return count;
     }
 
     @Override
     public String toString() {
-        return "Машина №" + id +"; марка:" + brand + "; модель:" + model + "; объем двигателя: " + engineVolume +
-                "; цвет: " + color + "; год выпуска: " + year + "; страна производитель - " + country + ".";}
+        return "Машина №" + id + "; марка:" + brand + "; модель:" + model + "; объем двигателя: " + engineVolume +
+                "; цвет: " + color + "; год выпуска: " + year + "; страна производитель - " + country + ".";
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Car car = (Car) o;
-        return Float.compare(car.engineVolume, engineVolume) == 0 && year == car.year && id == car.id && brand.equals(car.brand) && model.equals(car.model) && color.equals(car.color) && country.equals(car.country);
+        return Float.compare(car.engineVolume, engineVolume) == 0 && year == car.year && id == car.id && Objects.equals(brand, car.brand) && Objects.equals(model, car.model) && Objects.equals(color, car.color) && Objects.equals(country, car.country);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(brand, model, engineVolume, color, year, country, id);
+    }
+
+    public Car(String brand, String model, float engineVolume, String color, int year, String country) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+        count++;
+        if (brand == null || brand.isEmpty() || brand.equals("")) {
+            this.brand = "Вentley";
+        }
+        if (model == null || model.isEmpty() || model.equals("")) {
+            this.model = "V66";
+        }
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.6f;
+        }
+        if (color == null || color.isEmpty() || color.equals("")) {
+            this.color = "Белый";
+        }
+        if (year <= 0 || year > 2023) {
+            this.year = 2000;
+        }
+        if (country == null || country.isEmpty() || country.equals("")) {
+            this.country = "Англия";
+        }
     }
 }
 
--- src/CarList.java
+++ src/CarList.java
@@ -6,11 +6,10 @@ public class CarList {
         cars[1] = new Car("AUDI", "A8 50 L TDI", 3.0f, "Черный", 2020, "Германия");
         cars[2] = new Car("BMW", "Z8", 3.0f, "Черный", 2021, "Германия");
         cars[3] = new Car("Kia", "Sportage 4", 2.4f, "Красный", 2018, "Южная корея");
-        cars[4] = new Car("Hyandai", "Avante", 1.6f, "Красный", 2016, "Южная корея");
+        cars[4] = new Car("", "Avante", -1.2f, "", -15, "");
     }
 
     public void printCarList() {
-
         for (int i = 0; i < Car.getCount(); i++) {
             System.out.println(cars[i]);
         }
